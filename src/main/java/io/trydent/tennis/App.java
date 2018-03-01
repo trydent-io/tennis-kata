bc8b7b26f7eae99dae975226fc58877892c1472f
TryIO <io.trydent@gmail.com>
2018-03-01 16:20:27 +0100

    initialized maven project

---
+++ src/main/java/io/trydent/tennis/App.java
@@ -0,0 +1,13 @@
+package io.trydent.tennis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
